628930c430f22667e08102f47ab34cf747948f13
Phil Gebauer <philgebauer@Phils-Laptop.local>
2023-03-15 20:25:14 -0500

    iniital rough form

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,38 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useState } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  const [markdownObj, setMarkdownObj] = useState([]);
+  // const [htmlString, setHtmlString] = useState({});
+
+  function grabData() {
+        var elements = document.getElementById("myForm").elements;
+        var obj ={};
+          for(var i = 0 ; i < elements.length ; i++){
+              var item = elements.item(i);
+              obj[item.name] = item.value;
+          }
+        const propertyValues = Object.values(obj);
+
+
+        setMarkdownObj(propertyValues);
+        document.getElementById("myForm").reset();
+        // generateString();
+        console.log('markdown obj', markdownObj)
+    }
+
+    // function generateString() {
+    //   const propertyValues = Object.values(markdownObj);
+    //   setHtmlString(propertyValues)
+    //   console.log('generated string', generateString)
+    // }
+
+
   return (
     <>
       <Head>
@@ -15,108 +43,22 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+          <form id="myForm" action="/my-handling-form-page" method="post">
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
+                <label htmlFor="name">Name:</label>
+                <input type="text" id="name" name="user_name" />
 
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
+                <label htmlFor="mail">Email:</label>
+                <input type="email" id="mail" name="user_email" />
 
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
+                <label htmlFor="msg">Message:</label>
+                <textarea id="msg" name="user_message"></textarea>
 
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
+          </form>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        <button onClick={() => grabData()}> Hello World </button>
+        <div> {markdownObj} </div> 
+      
       </main>
     </>
   )
